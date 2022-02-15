cd530c506d86d9ea9cf924675e465d6fa39c2851
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-02-15 11:04:54 +0500

    Time: 45 ms (57.86%), Space: 27.6 MB (63.84%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int binarySearch(vector<int>& nums,int left, int right,int target){
+        if(left > right)
+            return -1;
+        int midpoint = (left + right) / 2;
+        if(nums[midpoint] == target)
+            return midpoint;
+        if(target > nums[midpoint])
+           return  binarySearch(nums,midpoint + 1,right,target);
+        return binarySearch(nums,left,midpoint - 1,target);
+    }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums,0,nums.size()-1,target);
+    }
+};
