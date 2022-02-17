5e2333952a0017fced0e6f38f53dec8361348157
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-02-18 01:29:52 +0500

    Time: 0 ms (100.00%), Space: 7.6 MB (48.63%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        int aliceScore = 0;
-        int bobScore = 0;
-        int left = 0;
-        int right = piles.size() -1;
-        sort(piles.begin(),piles.end());
-        for(int i=0;i<piles.size();i++){
-            if(piles[left] > piles[right] && i % 2 == 0){
-                aliceScore += piles[left];
-                left++;
-            }else if( i % 2 == 0){
-                aliceScore += piles[right];
-                right--;
-            }else if(piles[left] > piles[right]){
-                bobScore += piles[left];
-                left++;
-            }else{
-                bobScore += piles[right];
-                right--;
-            }
-        }
-        return aliceScore > bobScore ? 1 : 0;
-    }
+   bool stoneGame(vector<int> &piles)
+{
+   return true;
+};
+    
 };
