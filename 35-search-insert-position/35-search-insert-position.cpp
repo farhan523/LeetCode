d1bb83c87d3fe402bb6a0f6e2984d52d01b33177
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-02-19 10:04:05 +0500

    Time: 10 ms (22.75%), Space: 9.7 MB (23.23%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    
+    int binarySearch(vector<int>& arr,int l,int r,int x){
+        
+               if (r >= l) {
+                int mid = l + (r - l) / 2;
+
+                // If the element is present at the middle
+                // itself
+                if (arr[mid] == x)
+                    return mid;
+
+                // If element is smaller than mid, then
+                // it can only be present in left subarray
+                if ((arr[mid] > x && arr[l] <= x) && l != r)
+                    return binarySearch(arr, l, mid - 1, x);
+
+                // Else the element can only be present
+                // in right subarray
+                 if(arr[r] >= x &&  r -l >1)
+                        return binarySearch(arr, mid + 1, r, x);
+                   if (arr[l] < x && arr[r] > x)
+                        return l + 1;
+                    else if(arr[l] > x)
+                        return l;
+                     else if(arr[r] < x )
+                        return r + 1;
+                        return l + 1;
+            }
+        if(r < l){
+            if(arr[0] < x)
+                return 1;
+            return 0;
+        }
+        
+       if(arr[l] > x)
+           return l;
+        else return r + 1;
+       
+    }
+    
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums[0] > target)
+            return 0;
+        if(nums[nums.size() -1] < target)
+                return nums.size();
+      return binarySearch(nums,0,nums.size() -1, target);
+    }
+};
