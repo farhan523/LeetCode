d36059e9bd67a4be9ffcf285fea636bd6b274714
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-03-01 16:50:01 +0500

    Time: 4 ms (49.44%), Space: 9.8 MB (39.36%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -1,45 +1,56 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-     void isSym(TreeNode*&root){
-        if(root == NULL)
-            return;
-        if(root->left == NULL && root->right == NULL)
+class Solution
+{
+    public:
+        void isSym(TreeNode* &root)
+        {
+            if (root == NULL)
                 return;
-         TreeNode* temp;
-         if(root->left != NULL && root->right != NULL){
-             temp = root->left;
-             root->left = root->right;
-             root->right = temp;
-             isSym(root->left);
-             isSym(root->right);
-         }else if(root->left == NULL){
-             root->left = root->right;
-             root->right = NULL;
-             isSym(root->left);
-         }else if(root->right == NULL){
-             root->right = root->left;
-             root->left = NULL;
-             isSym(root->right);           
+            if (root->left == NULL && root->right == NULL)
+                return;
+            TreeNode * temp;
+            if (root->left != NULL && root->right != NULL)
+            {
+                temp = root->left;
+                root->left = root->right;
+                root->right = temp;
+                isSym(root->left);
+                isSym(root->right);
+            }
+            else if (root->left == NULL)
+            {
+                root->left = root->right;
+                root->right = NULL;
+                isSym(root->left);
+            }
+            else if (root->right == NULL)
+            {
+                root->right = root->left;
+                root->left = NULL;
+                isSym(root->right);
+            }
+        }
 
-         }
-       
-    }
-    
-    TreeNode* invertTree(TreeNode* root) {
-        if(root == NULL )
-            return root;
-         isSym(root);
+    TreeNode* invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+                return root;
+            TreeNode * temp;
+             temp = root->left;
+                root->left = root->right;
+                root->right = temp;
+                isSym(root->left);
+                isSym(root->right);
         return root;
     }
 };
