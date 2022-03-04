67fd9cc75c4a15ec0980263a921e58732bc9c3f0
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-03-04 11:37:51 +0500

    Time: 25 ms (9.72%), Space: 15.7 MB (18.37%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,27 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        
-        vector<int>res;
-        int lPtr = 0;
-        int rPtr = numbers.size() - 1;
-        while(lPtr < rPtr){
-            if(numbers[lPtr] + numbers[rPtr] == target){
-                res.push_back(lPtr + 1);
-                res.push_back(rPtr + 1);
-                return res;
-            }else if(numbers[rPtr] > target)
-                rPtr--;
-            else if(numbers[rPtr] + numbers[lPtr] > target){
-                rPtr--;
-            }else{
-                lPtr++;
+class Solution
+{
+    public:
+        vector<int> twoSum(vector<int> &numbers, int target)
+        {
+
+            vector<int> res;
+            int lPtr = 0;
+            int rPtr = numbers.size() - 1;
+            while (lPtr < rPtr)
+            {
+                if (numbers[lPtr] + numbers[rPtr] == target)
+                {
+                    res.push_back(lPtr + 1);
+                    res.push_back(rPtr + 1);
+                    return res;
+                }
+                else if (numbers[rPtr] > target || numbers[rPtr] + numbers[lPtr] > target)
+                    rPtr--;
+                else
+                {
+                    lPtr++;
+                }
             }
+            return res;
         }
-        return res;
-    }
 };
