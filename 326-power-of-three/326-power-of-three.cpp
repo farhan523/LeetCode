647e8d1e2918ed65118999f2661cf140dcb65519
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-03-05 17:10:12 +0500

    Time: 43 ms (23.46%), Space: 6.2 MB (8.30%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,12 +1,13 @@
-class Solution {
-public:
-    bool isPowerOfThree(int n) {
-         if(n <=0 )
-            return false;
-       int b = log10(n) / log10(3);
-        cout<<b;
-        if(pow(3,b) == n)
-            return true;
-        return false;
-    }
+class Solution
+{
+    public:
+        bool isPowerOfThree(int n)
+        {
+            if (n <= 0)
+                return false;
+            int b = log10(n) / log10(3);
+            cout << b;
+            return pow(3, b) == n;
+              
+        }
 };
