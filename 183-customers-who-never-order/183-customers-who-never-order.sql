1c7459b31549daa8bd25bb9614bf405baab26bc3
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-04-30 13:50:08 +0500

    Time: 910 ms (6.23%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,6 +1,5 @@
-select customers.name as 'Customers'
-from customers
-where customers.id not in
-(
-    select customerid from orders
-);
+select name as Customers
+from Customers c 
+left join  orders o
+on c.id = o.customerid
+where o.customerid is null
