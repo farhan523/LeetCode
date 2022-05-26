efec0614ddeaa79747a521be524d0945bdc31e0e
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-05-26 19:02:12 +0500

    Time: 134 ms (17.42%), Space: 36 MB (23.47%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -6,30 +6,31 @@ class Solution
             vector<vector < int>> res;
             vector<int> res1;
             vector<int> res2;
-           unordered_set<int>st(nums1.begin(), nums1.end());
+            unordered_set<int> st(nums1.begin(), nums1.end());
             for (int i = 0; i < nums2.size(); i++)
             {
                 auto it = st.find(nums2[i]);
-                if (it == st.end()){
+                if (it == st.end())
+                {
                     res2.push_back(nums2[i]);
-                   
                 }
-                  st.insert(nums2[i]);   
+                st.insert(nums2[i]);
             }
-            
-            unordered_set<int>st2(nums2.begin(), nums2.end());
+
+            unordered_set<int> st2(nums2.begin(), nums2.end());
             for (int i = 0; i < nums1.size(); i++)
             {
                 auto it = st2.find(nums1[i]);
-                if (it == st2.end()){
+                if (it == st2.end())
+                {
                     res1.push_back(nums1[i]);
-                  
                 }
-                 st2.insert(nums1[i]);   
+                st2.insert(nums1[i]);
             }
-           
-            
-            res = {res1,res2};
+
+            res = { res1,
+                res2
+            };
             return res;
         }
 };
