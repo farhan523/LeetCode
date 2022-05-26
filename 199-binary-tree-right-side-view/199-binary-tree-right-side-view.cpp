edb34d998d1d18bb72b4dd67967cb7d2c2ff9fbb
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-05-26 22:20:00 +0500

    Time: 8 ms (24.50%), Space: 12.3 MB (9.58%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -14,12 +14,13 @@ class Solution
     vector<int> res;
 
     public:
-        vector<int> rightSideView(TreeNode *root) {
-             if(root == NULL)
+        vector<int> rightSideView(TreeNode *root)
+        {
+            if (root == NULL)
                 return res;
-             queue<TreeNode*> q;
+            queue<TreeNode*> q;
             vector<vector < int>> result;
-           
+
             q.push(root);
             while (!q.empty())
             {
@@ -37,7 +38,8 @@ class Solution
 
                 result.push_back(temp);
             }
-            for(int i=0;i<result.size();i++){
+            for (int i = 0; i < result.size(); i++)
+            {
                 res.push_back(result[i][result[i].size() - 1]);
             }
             return res;
