197b8bc809a42b9491d463eb672610f7bc1dd9da
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-05-26 22:03:46 +0500

    Time: 23 ms (23.60%), Space: 22.7 MB (5.07%) - LeetHub

---
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,40 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+        int findBottomLeftValue(TreeNode *root)
+        {
+            queue<TreeNode*> q;
+            vector<vector < int>> result;
+            
+            q.push(root);
+            while (!q.empty())
+            {
+                vector<int> temp;
+                int k = q.size();
+                for (int i = 0; i < k; i++)
+                {
+                    temp.push_back(q.front()->val);
+                    if (q.front()->left != NULL)
+                        q.push(q.front()->left);
+                    if (q.front()->right != NULL)
+                        q.push(q.front()->right);
+                    q.pop();
+                }
+
+                result.push_back(temp);
+            }
+
+            return result[result.size() - 1][0];
+        }
+};
