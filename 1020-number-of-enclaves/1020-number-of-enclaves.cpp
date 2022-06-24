ab65d3e66618a2d2f1264eab66b9c26157f693b9
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-06-24 11:30:39 +0500

    Time: 87 ms (54.95%), Space: 21.7 MB (56.39%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool isClosedIsland = true;
+    int row;
+    int col;
+    int num = 0;
+    
+    void depthFirstSearch(vector<vector < int>> &grid, int r, int c)
+    {
+        if (grid[r][c] == 0 || grid[r][c] == 2)
+            return;
+        if (r + 1 >= row || c + 1 >= col || r - 1 < 0 || c - 1 < 0)
+            isClosedIsland = false;
+        grid[r][c] = 2;
+        num++;
+        if (r + 1 < row)
+            depthFirstSearch(grid, r + 1, c);
+        if (c + 1 < col)
+            depthFirstSearch(grid, r, c + 1);
+        if (r - 1 >= 0)
+            depthFirstSearch(grid, r - 1, c);
+        if (c - 1 >= 0)
+            depthFirstSearch(grid, r, c - 1);
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+         int count = 0;
+            row = grid.size();
+            col = grid[0].size();
+            for (int i = 1; i < grid.size() - 1; i++)
+            {
+                for (int j = 1; j < grid[0].size() - 1; j++)
+                {
+                    if (grid[i][j] != 2 and grid[i][j] != 0)
+                    {
+                        depthFirstSearch(grid, i, j);
+                        if (isClosedIsland){
+                            count += num;
+                        }
+                           num = 0;
+                        isClosedIsland = true;
+                    }
+                }
+            }
+            return count;
+    }
+};
