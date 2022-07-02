a495291180b03982404cb133427f299b88696d42
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-07-02 07:17:28 +0500

    Time: 64 ms (60.38%), Space: 35.1 MB (40.64%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,37 +1,34 @@
-class Solution {
-    queue<int>que;
-    unordered_set<int>st;
-public:
-    bool canReach(vector<int>& arr, int start) {
-        if( arr[start] == 0)
-            return true;
-        if(start + arr[start] < arr.size()){
-              que.push(start + arr[start]);
-            st.insert(start + arr[start]);
-        }
-          
-        if(start - arr[start] >= 0){
-            st.insert(start - arr[start]);
-            que.push(start - arr[start]);
-        }
-            
-        while(!que.empty()){
-             if(arr[que.front()] == 0)
+class Solution
+{
+    queue<int> que;
+    unordered_set<int> st;
+    public:
+        bool canReach(vector<int> &arr, int start)
+        {
+            if (arr[start] == 0)
+                return true;
+           que.push(start);
+            st.insert(start);
+
+            while (!que.empty())
+            {
+                if (arr[que.front()] == 0)
                     return true;
-            start = que.front();
-            if(start + arr[start] < arr.size() and st.find(start + arr[start]) == st.end()){
-                st.insert(start + arr[start]);
-                que.push(start + arr[start]);
+                start = que.front();
+                if (start + arr[start] < arr.size() and st.find(start + arr[start]) == st.end())
+                {
+                    st.insert(start + arr[start]);
+                    que.push(start + arr[start]);
+                }
+
+                if (start - arr[start] >= 0 and st.find(start - arr[start]) == st.end())
+                {
+                    st.insert(start - arr[start]);
+                    que.push(start - arr[start]);
+                }
+
+                que.pop();
             }
-                
-            if(start - arr[start] >= 0 and st.find(start - arr[start]) == st.end()){
-                 st.insert(start - arr[start]);
-                que.push(start - arr[start]);
-            }
-                
-           
-            que.pop();
+            return false;
         }
-        return false;
-    }
 };
