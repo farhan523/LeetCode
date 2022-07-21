67bc77689d0d1f79c53b5deca39e9aad5be2825d
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-07-21 17:24:07 +0500

    Time: 31 ms (29.39%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,53 +1,85 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *};
  */
-class Solution {
-    
-    vector<int>ancestor1;
-    vector<int>ancestor2;
-    bool found = false;
-    unordered_map<int,TreeNode*>mp;
-    void depthFirstSearch(TreeNode* root,TreeNode* p,vector<int>&ancestor){
-        if(root == NULL)
-            return;
-         ancestor.push_back(root->val);
-        if(mp.find(root->val) == mp.end())
-                mp.insert({root->val,root});
-        if(root->val == p->val){
-           
-            found = true;
-            return;
-        }
-         depthFirstSearch(root->left,p,ancestor);
-        if(found)
-            return;
-       
-        depthFirstSearch(root->right,p,ancestor);
-        if(found)
-            return;
-        ancestor.pop_back();
-        
-            
+
+// return 1 if found p 
+// return 2 if found q
+// return 0 if found both
+// return -1 if none
+class Solution
+{
+
+    TreeNode *result = NULL;
+
+    int depthFirstSearch(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if (root == NULL)
+        return -1;
+    int leftFound, rightFound;
+    leftFound = depthFirstSearch(root->left, p, q);
+    rightFound = depthFirstSearch(root->right, p, q);
+
+    if (leftFound == -1 && rightFound == -1 && root->val != p->val && root->val != q->val)
+        return -1;
+    if ((leftFound == 1 && rightFound == 2) || (leftFound == 2 && rightFound == 1))
+    {
+        if (result == NULL)
+            result = root;
+        return 0;
     }
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        depthFirstSearch(root,p,ancestor1);
-        found = false;
-        depthFirstSearch(root,q,ancestor2);
-        set<int>st(ancestor1.begin(),ancestor1.end());
-        for(int i=ancestor2.size()-1;i>-1;i--){
-            if(st.find(ancestor2[i]) != st.end()){
-                return mp.find(ancestor2[i])->second;
-                break;
-            }
-                
-        }
-        return root;
+
+    if (leftFound == -1 && rightFound == -1)
+    {
+        if (root->val == p->val)
+            return 1;
+        return 2;
     }
+
+    if (leftFound == -1)
+    {
+        if (root->val == q->val && rightFound == 1)
+        {
+            if (result == NULL)
+                result = root;
+            return 0;
+        }
+        if (root->val == p->val && rightFound == 2)
+        {
+            if (result == NULL)
+                result = root;
+            return 0;
+        }
+        return rightFound;
+    }
+
+    if (rightFound == -1)
+    {
+        if (root->val == q->val && leftFound == 1)
+        {
+            if (result == NULL)
+                result = root;
+            return 0;
+        }
+        if (root->val == p->val && leftFound == 2)
+        {
+            if (result == NULL)
+                result = root;
+            return 0;
+        }
+        return leftFound;
+    }
+        return 22;
+}
+    public:
+        TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+        {
+            depthFirstSearch(root, p, q);
+            return result;
+        }
 };
