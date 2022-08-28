a27854a6e02cfa4fcd0f8bc2db40a0da9e78ccca
Farhan Asghar <62025759+farhan523@users.noreply.github.com>
2022-08-28 07:41:22 +0500

    Time: 234 ms (84.97%), Space: 64.5 MB (79.92%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -3,7 +3,6 @@ class Solution
     queue<int> q;
     bool bipitrate(vector<vector < int>> &list, int index, vector< int > &visited)
     {
-        
         visited[index] = 1;
         q.push(index);
 
@@ -13,13 +12,17 @@ class Solution
             q.pop();
             for (int i = 0; i < list[top].size(); i++)
             {
-                if(visited[list[top][i]] == visited[top] )return false;
                 if (visited[list[top][i]] == -1)
                 {
                     visited[top] == 1 ? visited[list[top][i]] = 2 : visited[list[top][i]] = 1;
                     q.push(list[top][i]);
                 }
-               
+                else
+                {
+                    bool res = visited[list[top][i]] == visited[top] ? true : false;
+                    if (res)
+                        return false;
+                }
             }
         }
 
@@ -32,12 +35,12 @@ class Solution
             vector<int> visited(n + 1, -1);
             vector<int> temp;
             int size = n + 1;
-           std::vector<std::vector<int>> list(n+1, temp);
-            for (int i = 0; i <graph.size(); i++)
+            std::vector<std::vector < int>> list(n + 1, temp);
+            for (int i = 0; i < graph.size(); i++)
             {
 
                 list[graph[i][0]].push_back(graph[i][1]);
-                 list[graph[i][1]].push_back(graph[i][0]);
+                list[graph[i][1]].push_back(graph[i][0]);
             }
             for (int i = 1; i <= n; i++)
             {
